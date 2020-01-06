9eb3d2db54e9aa9ee17ee673245b28eb09a7d65a
project-chodanta <project.chodanta@gmail.com>
2020-01-06 17:54:40 +0900

    CodeUp - 1

---
+++ CodeUp-1/CodeUp-1/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  CodeUp-1
+//
+//  Created by HMWORLD on 06/01/2020.
+//  Copyright © 2020 danta. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main() {
+    
+    printf("Hello");
+    
+    return 0;
+    
+}
